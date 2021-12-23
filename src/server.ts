73d0583747357c48887cd7bc809b42e6146b2092
Tiago Silva <tiago.toya@hotmail.com>
2021-12-22 22:27:41 -0300

    refactor: tuning error logging middleware

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ server.use(express.json())
 
 server.use('/api', routes)
 
-server.use((_error: Error, _request: Request, response: Response, _next: NextFunction) => {
+server.use((error: Error, _request: Request, response: Response, _next: NextFunction) => {
+  console.error(error.message)
   response.status(500).json({ message: 'internal error', status: 'error' })
 })
 
