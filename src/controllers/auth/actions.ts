6d0b0dd712ef38918b5b44b6c0b6740c49d6ae1b
Tiago Silva <tiago.toya@hotmail.com>
2021-12-29 23:35:55 -0300

    refactor: removing unused catch

--- src/controllers/auth/actions.ts
+++ src/controllers/auth/actions.ts
@@ -3,11 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 import asyncHandler from 'express-async-handler'
 
 export const register = ({ domains }: ControllersInjection) => asyncHandler(async (request: Request, response: Response, next: NextFunction) => {
-  try {
-    const { email, password } = request.body
-    const registeredUser = await domains.auth.register({ email, password })
-    response.status(201).json(registeredUser)
-  } catch (error) {
-    next(error)
-  }
+  const { email, password } = request.body
+  const registeredUser = await domains.auth.register({ email, password })
+  response.status(201).json(registeredUser)
 })
