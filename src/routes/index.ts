5453280b4648742435509b59bb9606343b1443fe
Tiago Silva <tiago.toya@hotmail.com>
2022-11-30 15:06:28 -0300

    feat: add health-check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import authRouter from './auth'
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
 router.use('/auth', authRouter)
+
+router.get('/health-check', (request: Request, response: Response) => {
+  return response.json({ status: 'OK' })
+})
 
 export default router
